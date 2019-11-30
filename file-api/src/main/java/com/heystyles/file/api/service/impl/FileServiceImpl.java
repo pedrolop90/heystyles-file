4f3f16fb73635eb480de6fedd26dab54e538d259
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-11-30 10:24:40 -0500

    ....

--- file-api/src/main/java/com/heystyles/file/api/service/impl/FileServiceImpl.java
+++ file-api/src/main/java/com/heystyles/file/api/service/impl/FileServiceImpl.java
@@ -1,20 +1,14 @@
 package com.heystyles.file.api.service.impl;
 
-import com.heystyles.common.exception.APIExceptions;
 import com.heystyles.common.service.impl.ServiceImpl;
 import com.heystyles.file.api.dao.FileDao;
 import com.heystyles.file.api.entity.FileEntity;
-import com.heystyles.file.api.message.MessageKeys;
 import com.heystyles.file.api.service.FileService;
 import com.heystyles.file.core.domain.File;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-import static org.springframework.context.i18n.LocaleContextHolder.getLocale;
 
 @Service
 public class FileServiceImpl
@@ -23,9 +17,6 @@ public class FileServiceImpl
     @Autowired
     private FileDao fileDao;
 
-    @Autowired
-    private MessageSource messageSource;
-
     @Override
     protected CrudRepository<FileEntity, Long> getDao() {
         return fileDao;
@@ -33,11 +24,6 @@ public class FileServiceImpl
 
     @Override
     public File getFile(Long fileId) {
-        return Optional.ofNullable(findById(fileId))
-                .orElseThrow(() -> APIExceptions.objetoNoEncontrado(
-                        messageSource.getMessage(MessageKeys.FILE_NOT_FOUND,
-                                new String[]{String.valueOf(fileId)},
-                                getLocale())
-                ));
+        return findById(fileId);
     }
 }
